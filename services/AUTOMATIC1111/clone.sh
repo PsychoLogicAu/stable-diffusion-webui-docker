9649f5cc9e6fd646c088732b2eb40d95f4d1b8e3
PsychoLogicAu <psycho.logic.au@gmail.com>
2024-04-14 10:30:16 +1000

    reword me, update webui version

--- services/AUTOMATIC1111/clone.sh
+++ services/AUTOMATIC1111/clone.sh
@@ -2,10 +2,25 @@
 
 set -Eeuox pipefail
 
-mkdir -p /repositories/"$1"
-cd /repositories/"$1"
+repository_name="$1"
+repository_url="$2"
+tag_or_sha="$3"
+
+mkdir -p "/repositories/$repository_name"
+cd "/repositories/$repository_name"
+
 git init
-git remote add origin "$2"
-git fetch origin "$3" --depth=1
-git reset --hard "$3"
+git remote add origin "$repository_url"
+git fetch origin "$tag_or_sha" --depth=1
+
+# Check if the provided argument is a tag or a commit hash
+if git rev-parse "$tag_or_sha" >/dev/null 2>&1; then
+    # If it's a commit hash
+    git reset --hard "$tag_or_sha"
+else
+    # If it's a tag
+    git checkout "$tag_or_sha"
+fi
+
+# Remove the git directory to clean up
 rm -rf .git
